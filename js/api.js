7433ce0be4583b5fdf083a132e77353c8a495aa9
Kristoffer Dalby <kradalby@kradalby.no>
2014-07-17 21:31:01 +0200

    made create order work with events

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
 
-var APIURL = "http://localhost:8080/api/v1/";
+var APIURL = "http://localhost:8000/api/v1/";
 var APIUSER = "kradalby";
-var APIKEY = "daa3d71d9fae3cf16e627f19ad091a73c5586e37";
+var APIKEY = "9eab4a3e4a1fabd8772998d72b5360421e93d628";
